e5ccfa526e9e3cabaa993e02553768955321c726
Mario Andres Ravina <ayarmarxio@gmail.com>
2020-02-26 21:38:16 +0100

    lastcommit to save project

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,37 +7,57 @@ contract Election {
     struct Candidate {
         uint id;
         string name;
-        uint voteCount;
+        uint voteCount;               
     }
+
     // Store accounts that has voted
     mapping(address => bool) public voters;
-    // Store candidate    
-    // Fetch candidates 
+    // Store and Fetch candidate    
     mapping(uint => Candidate) public candidates;
-
+    // Store Finger print of the voter
+    string public hashFingerPrint;
     // Store candidates count
     uint public candidatesCount;
-
     // Constructor 
     function Election() public {
-        addCandidate("Candidate 1");
-        addCandidate("Candidate 2"); 
+        addCandidate("Yellow Candidate");
+        addCandidate("Orange Candidate"); 
     }
-
     function addCandidate (string _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
-    }
-
-    function vote (uint _candidateId) public {
+    }  
+    
+    function vote (uint _candidateId) public {        
         // require that they haven´t voted before
         require(!voters[msg.sender]);
-
+        // require(!Voter.hasVoted[msg.sender]);
         // require a a valid candidate
         require(_candidateId > 0 && _candidateId <= candidatesCount);
-        // record that voter has voted
-        voters[msg.sender] = true;        
         // update candidate vote count
         candidates[_candidateId].voteCount ++;
+        // record that voter has voted
+        voters[msg.sender] = true;
     }
+
+      // With INFURA and ITFS connection fopr fingerprint   
+    // function vote (uint _candidateId, string _fingerPrint) public {        
+    //     // require that they haven´t voted before
+    //     require(!voters[msg.sender]);
+    //     // require(!Voter.hasVoted[msg.sender]);
+    //     // require a a valid candidate
+    //     require(_candidateId > 0 && _candidateId <= candidatesCount);
+    //     // update candidate vote count
+    //     candidates[_candidateId].voteCount ++;
+    //     //record voterFingerPrint
+    //     hashFingerPrint = _fingerPrint;
+    //     // record that voter has voted
+    //     voters[msg.sender] = true;
+    // }
+    
+
+    // Method for getting the hash
+    // function getFingerPrint(address _key) public view returns (string){
+    //     return hashFingerPrint;
+    // }
 }
